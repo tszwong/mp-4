8c09d3a17a808fe8cebebb34c5a8f3295b6a8bfa
Tsz Kit Wong <wongt@bu.edu>
2024-11-03 18:48:57 -0500

    Update route.tsx

--- app/api/getWeatherData/route.tsx
+++ app/api/getWeatherData/route.tsx
@@ -16,6 +16,7 @@ export async function GET(req: Request) {
     const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next7days?unitGroup=us&include=days%2Ccurrent%2Cevent&key=${WEATHER_API_KEY}&contentType=json`);
 
     if (res.status !== 200) {
+        console.log("API Key:", process.env.API_KEY);
         return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
     }
 
